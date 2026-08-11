f0ef8536510757643a68231b2972844dd18d9f8a
Cauê Silva Dias <caue.silva.dias23@gmail.com>
2026-08-11 14:20:32 -0300

    feat: SecurityFilter

--- login-auth-api/src/main/java/com/example/repositories/UserRepository.java
+++ login-auth-api/src/main/java/com/example/repositories/UserRepository.java
@@ -1,11 +1,14 @@
 package com.example.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
-
+    Optional<User> findByEmail(String email);
     
+
 }
