bb4f83efb68e2014e2040ea68a775adedfec81d9
Cauê Silva Dias <caue.silva.dias23@gmail.com>
2026-08-11 15:24:59 -0300

    feat: SecurityConfig.java

---
+++ login-auth-api/src/main/java/com/example/infra/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    SecurityFilter securityFilter;
+
+    // Configura o filtro de segurança para interceptar as requisições e validar o token JWT.
+    // Liberando as rotas de login e registro para que não seja necessário autenticação para acessá-las.
+    // SessionCreationPolicy.STATELESS -> não vai criar sessão, cada requisição vai ser independente, não vai ter estado. Padrão para APIs RESTful.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+    }
+
+    // Configura o PasswordEncoder para criptografar a senha do usuário.
+    // Utiliza como dependencia no controler para criptografar a senha antes de salvar no banco de dados.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+    
+
+}
