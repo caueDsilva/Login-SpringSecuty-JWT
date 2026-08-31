3337473449888be438ca0c20b8da56d73e694ee7
Cauê Silva Dias <caue.silva.dias23@gmail.com>
2026-08-31 10:32:01 -0300

    feat: add SecurityConfig.java

--- login-auth-api/src/main/java/com/example/infra/security/SecurityConfig.java
+++ login-auth-api/src/main/java/com/example/infra/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
